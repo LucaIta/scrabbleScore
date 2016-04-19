7aa79227c2829aa326f260d3e4458b84c095e35f
Luca Quatela and Noah Kittleson <luca@>
2016-04-19 09:58:36 -0700

    passed final multiple letter test

--- src/main/java/ScrabbleScore.java
+++ src/main/java/ScrabbleScore.java
@@ -7,20 +7,29 @@ import static spark.Spark.*;
 
 
 public class ScrabbleScore {
-
   public static void main(String[] args) {
+
   }
 
   public static Integer calculateScore (String word) {
     Console myConsole = System.console();
     Map<String, Integer> scoreMap = new HashMap<String, Integer>();
     scoreMap.put("aeioulnrst", 1);
-    for (String key: scoreMap.keySet()) {
-      System.out.println(key);
-      if (key.contains(word)) {
-        return scoreMap.get(key);
+    scoreMap.put("dg", 2);
+    scoreMap.put("bcmp", 3);
+    scoreMap.put("fhvwy", 4);
+    scoreMap.put("k", 5);
+    scoreMap.put("jx", 8);
+    scoreMap.put("qz", 10);
+    Integer score = 0;
+    String[] letterArray = word.split("");
+    for (String letter: letterArray) {
+      for (String key: scoreMap.keySet()) {
+        if (key.contains(letter)) {
+          score += scoreMap.get(key);
+        }
       }
     }
-    return 0;
+    return score;
   }
 }
--- src/test/java/ScrabbleScoreTest.java
+++ src/test/java/ScrabbleScoreTest.java
@@ -5,9 +5,55 @@ import static org.junit.Assert.*;
 public class ScrabbleScoreTest {
 
   @Test
-  public void calculateScore_returnsScoreForSingleLetter_1(){
+  public void calculateScore_returnsScoreForSingleLetter_aeioulnrst(){
     ScrabbleScore testScrabble = new ScrabbleScore();
     Integer expected = 1;
     assertEquals(expected, testScrabble.calculateScore("a"));
+  }
+
+  @Test
+  public void calculateScore_returnsScoreForSingleLetter_dg(){
+    ScrabbleScore testScrabble = new ScrabbleScore();
+    Integer expected = 2;
+    assertEquals(expected, testScrabble.calculateScore("d"));
+  }
+
+  @Test
+  public void calculateScore_returnsScoreForSingleLetter_bcmp(){
+    ScrabbleScore testScrabble = new ScrabbleScore();
+    Integer expected = 3;
+    assertEquals(expected, testScrabble.calculateScore("p"));
+  }
+
+  @Test
+  public void calculateScore_returnsScoreForSingleLetter_fhvwy(){
+    ScrabbleScore testScrabble = new ScrabbleScore();
+    Integer expected = 4;
+    assertEquals(expected, testScrabble.calculateScore("v"));
+  }
+  @Test
+  public void calculateScore_returnsScoreForSingleLetter_k(){
+    ScrabbleScore testScrabble = new ScrabbleScore();
+    Integer expected = 5;
+    assertEquals(expected, testScrabble.calculateScore("k"));
+  }
+  @Test
+  public void calculateScore_returnsScoreForSingleLetter_jx(){
+    ScrabbleScore testScrabble = new ScrabbleScore();
+    Integer expected = 8;
+    assertEquals(expected, testScrabble.calculateScore("j"));
+  }
+  @Test
+  public void calculateScore_returnsScoreForSingleLetter_qz(){
+    ScrabbleScore testScrabble = new ScrabbleScore();
+    Integer expected = 10;
+    assertEquals(expected, testScrabble.calculateScore("q"));
+    assertEquals(expected, testScrabble.calculateScore("z"));
+  }
+  @Test
+  public void calculateScore_returnsScoreForMultipleLetters_zebra(){
+    ScrabbleScore testScrabble = new ScrabbleScore();
+    Integer expected = 16;
+    assertEquals(expected, testScrabble.calculateScore("zebra"));
   }
 }
